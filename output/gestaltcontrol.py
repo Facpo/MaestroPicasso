fe0f4f517bb6db31d966096e3175b23f2c209c74
francois <francoisauclair@hotmail.ca>
2017-10-08 23:26:29 -0400

    Changed to H-Bot configuration from pygestalt examples

--- output/gestaltcontrol.py
+++ output/gestaltcontrol.py
@@ -15,7 +15,6 @@ from pygestalt.publish import rpc	#remote procedure call dispatcher
 import time
 import io
 
-
 #------VIRTUAL MACHINE------
 class virtualMachine(machines.virtualMachine):
 	
@@ -24,18 +23,27 @@ class virtualMachine(machines.virtualMachine):
 		else: self.fabnet = interfaces.gestaltInterface('FABNET', interfaces.serialInterface(baudRate = 115200, interfaceType = 'ftdi', portName = '/dev/ttyUSB0'))
 		
 	def initControllers(self):
-		self.xAxisNode = nodes.networkedGestaltNode('X Axis', self.fabnet, filename = '086-005a.py', persistence = self.persistence)
-		self.yAxisNode = nodes.networkedGestaltNode('Y Axis', self.fabnet, filename = '086-005a.py', persistence = self.persistence)
-
+		print "init controllers, x and y"
+		self.xAxisNode = nodes.networkedGestaltNode('A axis', self.fabnet, filename = '086-005a.py', persistence = self.persistence)
+		self.yAxisNode = nodes.networkedGestaltNode('B axis', self.fabnet, filename = '086-005a.py', persistence = self.persistence)
 		self.xyNode = nodes.compoundNode(self.xAxisNode, self.yAxisNode)
+
 
 	def initCoordinates(self):
 		self.position = state.coordinate(['mm', 'mm'])
 	
 	def initKinematics(self):
-		self.xAxis = elements.elementChain.forward([elements.microstep.forward(4), elements.stepper.forward(1.8), elements.leadscrew.forward(8), elements.invert.forward(True)])
-		self.yAxis = elements.elementChain.forward([elements.microstep.forward(4), elements.stepper.forward(1.8), elements.leadscrew.forward(8), elements.invert.forward(False)])		
-		self.stageKinematics = kinematics.direct(2)	#direct drive on all axes
+    		# drive components of h-bot. Inputs are A/B stepper motors, outputs are X/Y in machine coordinates.
+    		# elements.elementChain.forward(
+        	# microstep => input microstepcount -> 1/microstepCount -> steps 
+        	# stepper =>   input stepAngle (degrees) -> stepAngle/360 -> revolutions
+        	# pulley =>    input pitchDiameter (mm)
+        	# invert =>    if it is inputted backwards. false = its correct connection])
+    		self.xAxis = elements.elementChain.forward([elements.microstep.forward(1), elements.stepper.forward(1.8), elements.pulley.forward(2.03), elements.invert.forward(False)])
+    		self.yAxis = elements.elementChain.forward([elements.microstep.forward(1), elements.stepper.forward(1.8), elements.pulley.forward(2.03), elements.invert.forward(False)])
+
+    		# define an H-BOT configuration
+    		self.stageKinematics = kinematics.hbot()        # add invertX = True, invertY = True if needed
 	
 	def initFunctions(self):
 		self.move = functions.move(virtualMachine = self, virtualNode = self.xyNode, axes = [self.xAxis, self.yAxis], kinematics = self.stageKinematics, machinePosition = self.position,planner = 'null')
